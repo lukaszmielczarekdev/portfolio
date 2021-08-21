5e4e7f675d007ff1239839f47a3178e052536f48
Lukasz Mielczarek <lukasz.mielczarek.dev@gmail.com>
2021-08-21 20:39:36 +0200

    PORTFOLIO-12 intro visibility

---
+++ utils/observer.js
@@ -0,0 +1,35 @@
+const intro = document.querySelector("#introArticle");
+const header = document.querySelector("#header");
+const username = document.querySelector("#username");
+
+const usernameOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-200px",
+};
+
+const fadeIn = () => {
+  intro.classList.remove("hidden");
+  intro.classList.add("visible");
+  header.classList.remove("visible");
+  header.classList.add("hidden");
+};
+
+const fadeOut = () => {
+  intro.classList.remove("visible");
+  intro.classList.add("hidden");
+  header.classList.remove("hidden");
+  header.classList.add("visible");
+};
+
+const usernameObserver = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      fadeOut();
+    } else {
+      fadeIn();
+    }
+  });
+}, usernameOptions);
+
+usernameObserver.observe(username);
